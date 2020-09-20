11c2f9e341f5468ad1699996cdcd77916ff6d3e6
Daniel Bowman <daniel@danielbowman.co.uk>
2020-09-20 17:42:36 +0100

    Whatis parsing

--- parse.rb
+++ parse.rb
@@ -4,7 +4,7 @@ require 'open3'
 INPUT=ARGV[0]
 TYPE=ARGV[1]
 
-data = File.read(INPUT)
+data = File.read('aws.json')
 a = JSON.parse(data)
 
 a['included'].each do |doc|
@@ -13,7 +13,7 @@ a['included'].each do |doc|
   section = attr['category']
   section = 'data' if attr['category'] == 'data_source'
   content = "#{attr['content']}"
-  content.gsub!(/# Resource: [^\n]*/, '')
+  content.gsub!(/^# Resource:/, "## NAME \n#{attr['title']} \\- #{attr['description']}")
   content.gsub!(/^#/, '')
   content.gsub!('page_title:', 'title:')
   stdout_str, exit_code = Open3.capture2("pandoc -f markdown -t man -s -o tf_#{TYPE}_#{doc['attributes']['category']}_#{doc['attributes']['slug']}.7", stdin_data: content)
